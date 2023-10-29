f0b4d83d58893181f76fb0833947a74634884867
Hotimpulse <jz2@rambler.ru>
2023-10-29 13:07:30 +0300

    feat: adding API fetching functionality and some UI to display the data

--- vite-project/src/components/PokemonComponent.tsx
+++ vite-project/src/components/PokemonComponent.tsx
@@ -3,6 +3,14 @@ import { MyButton } from './ButtonComponent';
 
 interface Pokemon {
   name: string;
+  species: string;
+  img: string;
+  type: string;
+  stats: {
+    hp: string;
+    attack: string;
+    defense: string;
+  };
 }
 
 interface State {
@@ -10,19 +18,66 @@ interface State {
   loading: boolean;
   error: string | null;
   searchData: string;
+  pokemonData: Pokemon;
 }
 
-export class PokemonComponent extends React.Component<{ name: string }, State> {
+export class PokemonComponent extends React.Component {
   state: State = {
     data: null,
     loading: true,
     error: null,
     searchData: '',
+    pokemonData: {
+      name: '',
+      species: '',
+      img: '',
+      type: '',
+      stats: {
+        hp: '',
+        attack: '',
+        defense: '',
+      },
+    },
   };
 
-  componentDidMount(): void {
-    this.fetchPokemonData();
-  }
+  fetchPokemonData = async (): Promise<void> => {
+    try {
+      const pokemonName: string = this.state.searchData.toLowerCase() || '';
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP request failed! Status: ${response.status}`);
+      }
+
+      const data: any = await response.json();
+
+      const pokemonData: Pokemon = {
+        name: data.name || '',
+        species: data.species.name || '',
+        img: data.sprites.front_default || '',
+        type: data.types[0]?.type.name || '',
+        stats: {
+          hp: data.stats[0]?.base_stat || '',
+          attack: data.stats[1]?.base_stat || '',
+          defense: data.stats[2]?.base_stat || '',
+        },
+      };
+
+      this.setState({
+        data: pokemonData,
+        loading: false,
+        error: null,
+      });
+    } catch (error) {
+      this.setState({
+        data: null,
+        loading: false,
+        error: 'Error fetching data from the server',
+      });
+    }
+  };
 
   handleChange = (event: FormEvent<HTMLInputElement>) => {
     this.setState({ searchData: event.currentTarget.value });
@@ -34,26 +89,61 @@ export class PokemonComponent extends React.Component<{ name: string }, State> {
     localStorage.setItem('searchedPokemon', this.state.searchData);
   };
 
-  fetchPokemonData = async () => {};
+  throwError = () => {
+    throw new Error('This is an error!');
+  };
 
   render() {
-    const { searchData } = this.state;
+    const { loading, error, data, searchData } = this.state;
     return (
       <>
-        <div className="bg-[#bada55] text-[22px] text-color-red h-96 flex-auto p-12 ">
-          <div className="flex-auto">
-            <span className="mr-5 flex">Search for a Pokemon:</span>
-            <form onSubmit={this.handleSubmit as React.FormEventHandler}>
+        <div className="bg-[#bada55] text-[22px] text-color-red h-full flex-auto p-12 w-full rounded">
+          <div className="flex-auto space-y-5">
+            <h2 className="mr-5 flex">Search for a Pokemon:</h2>
+            <form
+              className="space-y-5"
+              onSubmit={this.handleSubmit as React.FormEventHandler}
+            >
               <input
-                className="border 1px red flex-auto mr-5 text-blue-600"
+                className="border 1px red flex-auto mr-5 text-blue-600 w-full pl-2 rounded"
                 type="text"
                 name="search"
-                placeholder="Search for a Pokemon's name (i.e. Pikachu)"
+                placeholder="(i.e. Pikachu)"
                 value={searchData}
                 onChange={this.handleChange}
               />
               <MyButton label="Search" type="submit" />
             </form>
+          </div>
+          <div className="bg-[#40f083] text-3xl font-bold underline flex-auto h-auto p-12 space-y-5 rounded">
+            <div className="flex flex-col gap-10 h-full items-center">
+              <h2>Pokemon Details:</h2>
+              <p>
+                Name:
+                {data?.name &&
+                  `${data?.name?.charAt(0).toUpperCase()}${data?.name.slice(
+                    1
+                  )}`}
+              </p>
+              <p>Species: {data?.species}</p>
+              {data?.img && (
+                <img className="w-48" src={data?.img} alt="Pokemon image" />
+              )}
+              <p>Type: {data?.type}</p>
+              <p>Stats:</p>
+              <ul>
+                <li>HP: {data?.stats.hp}</li>
+                <li>Attack: {data?.stats.attack}</li>
+                <li>Defense: {data?.stats.defense}</li>
+              </ul>
+            </div>
+            <MyButton label="Error" onClick={this.throwError} />
+            {loading && <p>Getting data, wait a moment...</p>}
+            {error && (
+              <div>
+                <h2 className="text-red-800">{error}</h2>
+              </div>
+            )}
           </div>
         </div>
       </>
